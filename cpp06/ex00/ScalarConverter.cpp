8813f6b26aa8c1dfe2b7bfbd162085a3a0bcf2e0
Salome Brochard <salome.brchrd@gmail.com>
2026-07-24 13:10:57 +0200

    parsing

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,83 @@
 /*   By: sbrochar <sbrochar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/17 17:03:03 by sbrochar          #+#    #+#             */
-/*   Updated: 2026/07/23 17:28:55 by sbrochar         ###   ########.fr       */
+/*   Updated: 2026/07/24 13:10:28 by sbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter()
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+ScalarConverter::ScalarConverter(ScalarConverter const &other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+}
+ScalarConverter::~ScalarConverter()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+ScalarConverter &ScalarConverter::operator=(ScalarConverter const &other)
+{
+	(void)other;
+	return (*this);
+}
+
+void ScalarConverter::convert(const std::string value)
+{
+	if (value.compare("nan") == 0)
+	{
+		std::cout << "char : impossible" << std::endl;
+		std::cout << "int : impossible" << std::endl;
+		std::cout << "float : nanf" << std::endl;
+		std::cout << "double: nan" << std::endl;
+		return ;
+	}
+	else if (value.compare("nanf") == 0)
+	{
+		std::cout << "char : impossible" << std::endl;
+		std::cout << "int : impossible" << std::endl;
+		std::cout << "float : nanf" << std::endl;
+		std::cout << "double: nan" << std::endl;
+		return ;
+	}
+	else if (value.compare("+inf") == 0)
+	{
+		std::cout << "char : impossible" << std::endl;
+		std::cout << "int : impossible" << std::endl;
+		std::cout << "float : +inff" << std::endl;
+		std::cout << "double: +inf" << std::endl;
+		return ;
+	}
+	else if (value.compare("+inff") == 0)
+	{
+		std::cout << "char : impossible" << std::endl;
+		std::cout << "int : impossible" << std::endl;
+		std::cout << "float : +inff" << std::endl;
+		std::cout << "double: +inf" << std::endl;
+		return ;
+	}
+	else if (value.compare("-inf") == 0)
+	{
+		std::cout << "char : impossible" << std::endl;
+		std::cout << "int : impossible" << std::endl;
+		std::cout << "float : -inff" << std::endl;
+		std::cout << "double: -inf" << std::endl;
+		return ;
+	}
+	else if (value.compare("-inff") == 0)
+	{
+		std::cout << "char : impossible" << std::endl;
+		std::cout << "int : impossible" << std::endl;
+		std::cout << "float : -inff" << std::endl;
+		std::cout << "double: -inf" << std::endl;
+		return ;
+	}
+	else if (value.size() == 1 && (value[0] < '0' || value[0] > '9'))
+	{
+		
+	}
+	
+}
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,26 +6,25 @@
 /*   By: sbrochar <sbrochar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/17 17:03:09 by sbrochar          #+#    #+#             */
-/*   Updated: 2026/07/23 17:38:28 by sbrochar         ###   ########.fr       */
+/*   Updated: 2026/07/24 10:58:21 by sbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 # include <string>
+# include <iostream>
 
 class ScalarConverter
 {
   public:
-	ScalarConverter();
-	ScalarConverter(ScalarConverter const &other);
-	ScalarConverter(std::string const &name);
-	ScalarConverter(std::string const &name, int grade);
-	~ScalarConverter();
-	ScalarConverter &operator=(ScalarConverter const &other);
+	static void convert(const std::string value);
 
   private:
-	std::string const name;
+	ScalarConverter();
+	ScalarConverter(ScalarConverter const &other);
+	~ScalarConverter();
+	ScalarConverter &operator=(ScalarConverter const &other);
 };
 
 #endif
