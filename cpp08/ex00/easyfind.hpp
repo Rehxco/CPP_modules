362a41538a29b41aa4f994944dbb9db42ff7e8d8
Rehxco <salome.brchrd@gmail.com>
2026-08-17 19:40:32 +0200

    ex00

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbrochar <sbrochar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/17 17:03:09 by sbrochar          #+#    #+#             */
+/*   Updated: 2026/08/17 19:03:04 by sbrochar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <algorithm>
+# include <exception>
+
+class NotFoundException : public std::exception
+{
+  public:
+	virtual const char *what() const throw();
+};
+const char *NotFoundException::what() const throw()
+{
+	return ("Error: Number not found");
+}
+template <typename T> typename T::iterator easyfind(T &container, int n)
+{
+	typename T::iterator it = std::find(container.begin(), container.end(), n);
+	if (it == container.end())
+		throw NotFoundException();
+	return (it);
+}
+
+#endif
